ce2360cb83054777da4e1bb25e2536cf21a094e1
Victor Redondo <dev.victor.redondo@gmail.com>
2020-01-10 14:20:12 -0600

    Completed module

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Index'});
