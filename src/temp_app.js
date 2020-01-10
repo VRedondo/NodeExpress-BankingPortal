67443b5f877bb422db2effa7a6d55df5d64d850c
Victor Redondo <dev.victor.redondo@gmail.com>
2020-01-10 14:14:52 -0600

    Completed module

--- src/temp_app.js
+++ src/temp_app.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const express = require('express');
-
-const app = express();
+const app = require('express');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -13,6 +11,4 @@ app.get('/', function(req, res) {
   res.render('index', { title: 'Index'});
 });
 
-app.listen(3000, function() {
-  console.log('PS Project Running on port 3000!');
-});
+app.listen(3000);
